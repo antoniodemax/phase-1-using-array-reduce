dc43c45c7e6eddeef8ddcc71381fd6cfa9c6d080
Anthony <anthony.onyango@student.moringaschool.com>
2024-07-03 23:48:45 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//callback function
+
+// specified callback functons in the arrays are called
+const totalBatteries = batteryBatches.reduce((accumulator, currentBatch) => {
+    return accumulator + currentBatch;
+},
+ 0);
